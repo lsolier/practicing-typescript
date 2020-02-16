1ef170d442189934232be220c24c751d0df7eca8
lsolier <lsolier@github.com>
2020-02-16 14:13:47 -0500

    promises

--- lab001/app.js
+++ lab001/app.js
@@ -1,37 +1,14 @@
 "use strict";
 (function () {
-    //funcion asignada a una variable
-    var avenger = {
-        nombre: 'Steve',
-        clave: 'Capitan America',
-        poder: 'droga'
-    };
-    //extraer las propiedades de un objeto
-    var nombre = avenger.nombre;
-    console.log(nombre);
-    var extraer = function (avenger) {
-        var poder = avenger.poder;
-        console.log(poder);
-    };
-    extraer(avenger);
-    var extraer2 = function (_a) {
-        var clave = _a.clave;
-        console.log(clave);
-    };
-    extraer2(avenger);
-    //desestructurar un arreglo
-    var avengers = ['Thor', 'Iroman', 'Spiderman'];
-    console.log(avengers[0]);
-    var rayo = avengers[0], hombre = avengers[1], arana = avengers[2];
-    console.log(hombre);
-    var extraerArr = function (avengers) {
-        var nombre = avengers[0];
-        console.log(nombre);
-    };
-    extraerArr(avengers);
-    var extraerArr2 = function (_a) {
-        var nombre = _a[0];
-        console.log(nombre);
-    };
-    extraerArr2(avengers);
+    //promesas en javascript
+    console.log('inicio');
+    const prom1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject('se termino el timeout');
+        }, 1000);
+    });
+    prom1
+        .then(mensaje => console.log(mensaje))
+        .catch(error => console.warn(error));
+    console.log('fin');
 })();
